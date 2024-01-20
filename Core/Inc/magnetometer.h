1e79f74076159217b9c3f12bae1da5601eca31d7
Alexandr <142592899+alex-and-ye@users.noreply.github.com>
2024-01-21 04:37:36 +0600

    Update magnetometer.h

--- Core/Inc/magnetometer.h
+++ Core/Inc/magnetometer.h
@@ -1,106 +1,39 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.h
-  * @brief          : Header for main.c file.
-  *                   This file contains the common defines of the application.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-
-/* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __MAIN_H
-#define __MAIN_H
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-/* Includes ------------------------------------------------------------------*/
-#include "stm32l4xx_hal.h"
-
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Exported types ------------------------------------------------------------*/
-/* USER CODE BEGIN ET */
-
-/* USER CODE END ET */
-
-/* Exported constants --------------------------------------------------------*/
-/* USER CODE BEGIN EC */
-
-/* USER CODE END EC */
-
-/* Exported macro ------------------------------------------------------------*/
-/* USER CODE BEGIN EM */
-
-/* USER CODE END EM */
-
-/* Exported functions prototypes ---------------------------------------------*/
-void Error_Handler(void);
-
-/* USER CODE BEGIN EFP */
-
-#ifndef __MMC5983MA_H__
-#define __MMC5983MA_H__
+#ifndef __MAGNETOMETER_H__
+#define __MAGNETOMETER_H__
 
 #include "stm32l4xx_hal.h"
 
-// Function to write to MMC5983MA register
-// reg: Register address to write to
-// data: Data to write to the register
-void MMC5983MA_WriteReg(uint8_t reg, uint8_t data);
+#define MAGNETOMETER_SPI &hspi1 // SPI interface
+#define MAGNETOMETER_CS_PIN GPIO_PIN_4 // Chip select pin
+#define MAGNETOMETER_CS_GPIO_PORT GPIOA // Chip select port
 
-// Function to read from MMC5983MA register
-// reg: Register address to read from
-// Returns: Data read from the register
-uint8_t MMC5983MA_ReadReg(uint8_t reg);
+// MMC5983MA register addresses
+#define MAGNETOMETER_XOUT_L 0x00
+#define MAGNETOMETER_XOUT_H 0x01
+#define MAGNETOMETER_YOUT_L 0x02
+#define MAGNETOMETER_YOUT_H 0x03
+#define MAGNETOMETER_ZOUT_L 0x04
+#define MAGNETOMETER_ZOUT_H 0x05
+#define MAGNETOMETER_TEMP_L 0x06
+#define MAGNETOMETER_TEMP_H 0x07
+#define MAGNETOMETER_STATUS 0x08
+#define MAGNETOMETER_CONTROL_0 0x09
+#define MAGNETOMETER_CONTROL_1 0x0A
+#define MAGNETOMETER_CONTROL_2 0x0B
+#define MAGNETOMETER_PRODUCT_ID 0x2F
+
+// Function to write to MAGNETOMETER register
+void MAGNETOMETER_WriteReg(uint8_t reg, uint8_t data);
+
+// Function to read from MAGNETOMETER register
+uint8_t MAGNETOMETER_ReadReg(uint8_t reg);
 
 // Function to read magnetic field data
 // pData: Pointer to an array where the magnetic field data will be stored
-// The array should have 3 elements for X, Y, and Z data respectively
-void MMC5983MA_ReadMagneticField(int16_t* pData);
+// The array should have 3 elements for X, Y, and Z
+void MAGNETOMETER_ReadMagneticField(int16_t* pData);
 
-// Function to initialize MMC5983MA
-void MMC5983MA_Init(void);
+// Function to initialize MAGNETOMETER
+void MAGNETOMETER_Init(void);
 
-#endif // __MMC5983MA_H__
-
-/* USER CODE END EFP */
-
-/* Private defines -----------------------------------------------------------*/
-#define B1_Pin GPIO_PIN_13
-#define B1_GPIO_Port GPIOC
-#define USART_TX_Pin GPIO_PIN_2
-#define USART_TX_GPIO_Port GPIOA
-#define USART_RX_Pin GPIO_PIN_3
-#define USART_RX_GPIO_Port GPIOA
-#define TMS_Pin GPIO_PIN_13
-#define TMS_GPIO_Port GPIOA
-#define TCK_Pin GPIO_PIN_14
-#define TCK_GPIO_Port GPIOA
-#define SWO_Pin GPIO_PIN_3
-#define SWO_GPIO_Port GPIOB
-
-/* USER CODE BEGIN Private defines */
-
-/* USER CODE END Private defines */
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* __MAIN_H */
+#endif /* __MAGNETOMETER_H__ */
